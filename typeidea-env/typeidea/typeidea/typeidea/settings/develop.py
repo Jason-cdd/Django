a327899c21e9c1673b1e30cd329360c7306c0b8a
Jason-cdd <328491433@qq.com>
2020-09-05 15:55:56 +0800

    增加blog/config/comment三个app

---
+++ typeidea-env/typeidea/typeidea/typeidea/settings/develop.py
@@ -0,0 +1,13 @@
+from .base import *  # NOQA
+
+DEBUG = True
+
+# Database
+# https://docs.djangoproject.com/en/3.1/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',   # 此处和文章不一样
+    }
+}
